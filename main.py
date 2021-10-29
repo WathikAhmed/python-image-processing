9688be42eb56fc5e02d572a13bef577c794e25e8
WathikAhmed <zerox4224@gmail.com>
2021-10-30 07:34:44 +1100

    Add function to adjust value of image

--- main.py
+++ main.py
@@ -252,6 +252,46 @@ def saturation(readDirr,cropSection):
 
         #_____END_____#
 
+def value(readDirr,cropSection):
+  
+    for file in os.listdir(readDirr):
+        f_img = readDirr+"/"+file
+        MYDIR = ("saturation" + cropSection)
+        CHECK_FOLDER = os.path.isdir(MYDIR)
+
+        # If folder doesn't exist, then create it.
+        if not CHECK_FOLDER:
+            os.makedirs(MYDIR)
+            print("created folder : ", MYDIR)
+
+        fileName = "saturation" + cropSection + file
+        destination = os.path.join(os.getcwd(), MYDIR, fileName)
+        destination2 = os.path.join(currentDir, fileName)
+        #print("---------------------"+f_img)
+
+        #-----Reading the image-----------------------------------------------------
+        img = cv2.imread(f_img)
+        #cv2.imshow("img",img) 
+
+        #-----converted the values to float32 during BGR2HSV transformation to avoid negative values during saturation transformation to due uint8 (default) overflow----------------------------------- 
+        imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV).astype("float32")
+        #cv2.imshow("lab",lab)
+
+        #-----splitting hsv values, adjusting the individual channels and then doing a merge-------------------------
+        (h, s, v) = cv2.split(imghsv)
+        v = v*2
+        v = np.clip(v,0,255)
+        imghsv = cv2.merge([h,s,v])
+
+        #-----converted it back to default uint8 after my hue adjustment--------------------
+        imgrgb = cv2.cvtColor(imghsv.astype("uint8"), cv2.COLOR_HSV2BGR)
+        #cv2.imshow('final', imgrgb)
+        #cv2.waitKey(0)
+        cv2.imwrite(destination, imgrgb)
+        cv2.imwrite(destination2, imgrgb)
+
+        #_____END_____# 
+
 
 
 
