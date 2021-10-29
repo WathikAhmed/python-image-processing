10331dbe17df783d5628b75ae0a09e8fbaa1c0df
WathikAhmed <zerox4224@gmail.com>
2021-10-30 07:46:18 +1100

    Update main.py

--- main.py
+++ main.py
@@ -292,6 +292,55 @@ def value(readDirr,cropSection):
 
         #_____END_____# 
 
+def value(readDirr,cropSection):
+  
+    for file in os.listdir(readDirr):
+        f_img = readDirr+"/"+file
+        MYDIR = ("saturation" + cropSection)
+        CHECK_FOLDER = os.path.isdir(MYDIR)
+
+        # If folder doesn't exist, then create it.
+        if not CHECK_FOLDER:
+            os.makedirs(MYDIR)
+            print("created folder : ", MYDIR)
+
+        fileName = "saturation" + cropSection + file
+        destination = os.path.join(os.getcwd(), MYDIR, fileName)
+        destination2 = os.path.join(currentDir, fileName)
+        #print("---------------------"+f_img)
+
+        #-----Reading the image-----------------------------------------------------
+        img = cv2.imread(f_img)
+        #cv2.imshow("img",img) 
+
+        #-----Converting image to LAB Color model----------------------------------- 
+        lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        #cv2.imshow("lab",lab)
+
+        #-----Splitting the LAB image to different channels-------------------------
+        l, a, b = cv2.split(lab)
+        #cv2.imshow('l_channel', l)
+        #cv2.imshow('a_channel', a)
+        #cv2.imshow('b_channel', b)
+
+        #-----Applying CLAHE to L-channel-------------------------------------------
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+        cl = clahe.apply(l)
+        #cv2.imshow('CLAHE output', cl)
+
+        #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+        limg = cv2.merge((cl,a,b))
+        #cv2.imshow('limg', limg)
+
+        #-----Converting image from LAB Color model to RGB model--------------------
+        imgrgb = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+        #cv2.imshow('final', imgrgb)
+        #cv2.waitKey(0)
+        cv2.imwrite(destination, imgrgb)
+        cv2.imwrite(destination2, imgrgb)
+
+        #_____END_____# 
+
 
 
 
