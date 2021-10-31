f5e28157928d92d03510d8fbf824c14227cb181e
WathikAhmed <zerox4224@gmail.com>
2021-10-31 17:28:39 +1100

    Update processCroppedImages.py
    rm extra code

--- processCroppedImages.py
+++ processCroppedImages.py
@@ -4,7 +4,6 @@ import os
 import os.path
 import cv2 
 from PIL import Image
-import asyncio
 import sys
 import numpy as np
 
@@ -12,78 +11,6 @@ import numpy as np
 sourceFolder = "./source"
 sourceFolderCropped = "./sourceFolderCropped"
 currentDir="xxx"
-
-
-
-def cropTopLeft():
-    (left, upper, right, lower) = (0, 0, 1024, 1024)
-    
-    for file in os.listdir(sourceFolder):
-        f_img = sourceFolder+"/"+file
-        img = Image.open(f_img)
-        # Cropping
-        img = img.crop((left, upper, right, lower))
-        # You should change 'test' to your preferred folder.
-        MYDIR = ("sourceFolderCropped")
-        CHECK_FOLDER = os.path.isdir(MYDIR)
-
-        # If folder doesn't exist, then create it.
-        if not CHECK_FOLDER:
-            os.makedirs(MYDIR)
-            print("created folder : ", MYDIR)
-
-        # You should change 'test' to your preferred folder.
-        MYDIR2 = ("Final")
-        CHECK_FOLDER = os.path.isdir(MYDIR2)
-
-        # If folder doesn't exist, then create it.
-        if not CHECK_FOLDER:
-            os.makedirs(MYDIR2)
-            print("created folder : ", MYDIR2)
-        
-        newDir = os.path.join(os.getcwd(), MYDIR)
-        global sourceFolderCropped
-        global currentDir
-        sourceFolderCropped = newDir
-        currentDir = os.path.join(os.getcwd(), "Final")
-
-        file = "cropTopLeft" + file
-        destination = os.path.join(os.getcwd(), MYDIR, file)
-        destination2 = os.path.join(currentDir, file)
-        #print(destination)
-        img.save(destination)
-        img.save(destination2)
-
-def cropBottomLeft():
-    (left, upper, right, lower) = (0, 1568, 1024, 2592)
-  
-    for file in os.listdir(sourceFolder):
-        f_img = sourceFolder+"/"+file
-        img = Image.open(f_img)
-        # Cropping
-        img = img.crop((left, upper, right, lower))
-        # You should change 'test' to your preferred folder.
-        MYDIR = ("sourceFolderCropped")
-        CHECK_FOLDER = os.path.isdir(MYDIR)
-
-        # If folder doesn't exist, then create it.
-        if not CHECK_FOLDER:
-            os.makedirs(MYDIR)
-            print("created folder : ", MYDIR)
-
-        newDir = os.path.join(os.getcwd(), MYDIR)
-        global sourceFolderCropped 
-        sourceFolderCropped = newDir
-        #print(sourceFolderCropped)
-
-        file = "cropBottomLeft" + file
-        destination = os.path.join(os.getcwd(), MYDIR, file)
-        #print(destination)
-        destination2 = os.path.join(currentDir, file)
-        #print(destination)
-        img.save(destination)
-        img.save(destination2)
-
 
 
 def detectGreen(readDirr):
@@ -343,22 +270,17 @@ def value(readDirr):
 
 
 
-#print(len(sys.argv))
-#print(str(sys.argv))
 
-
-async def main():
-
-    # Detect Green
-    detectGreen(sourceFolderCropped)
+# Detect Green
+detectGreen(sourceFolderCropped)
     
-    # Increse Green in RGB
-    increaseGreen(sourceFolderCropped)
+# Increse Green in RGB
+increaseGreen(sourceFolderCropped)
 
-    # Increse Hue
-    hue(sourceFolderCropped)
+# Increse Hue
+hue(sourceFolderCropped)
 
-    # Increse Saturation
-    saturation(sourceFolderCropped)
+# Increse Saturation
+saturation(sourceFolderCropped)
 
-asyncio.run(main())
+print("Done!")
